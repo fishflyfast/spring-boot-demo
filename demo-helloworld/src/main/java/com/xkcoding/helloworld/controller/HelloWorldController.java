fa448687f9dd6e6c39880f0f155e6e8d48326c20
zhaokm <fishflyfast@aliyun.com>
2021-09-05 20:47:03 +0800

    1.升级springboot版本到2.5.4 2.将Controller类单独放一个包 3.增加banner

---
+++ demo-helloworld/src/main/java/com/xkcoding/helloworld/controller/HelloWorldController.java
@@ -0,0 +1,25 @@
+package com.xkcoding.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import cn.hutool.core.util.StrUtil;
+
+@RestController
+public class HelloWorldController {
+
+	/**
+     * Hello，World
+     *
+     * @param who 参数，非必须
+     * @return Hello, ${who}
+     */
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(required = false, name = "who") String who) {
+        if (StrUtil.isBlank(who)) {
+            who = "World";
+        }
+        return StrUtil.format("Hello, {}!", who);
+    }
+}
